516baae8b6238b770618f42d92508ef00b617292
Chris Santangeli <chrissantangeli@Chriss-MacBook-Pro-2.local>
2024-09-09 12:23:12 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -18,7 +18,7 @@ class FizzBuzz {
 
             } else if (divisibleBy3) {
 
-                System.out.println("Fizz");
+                System.out.println("Fizzed");
 
             } else if (divisibleBy5) {
 
