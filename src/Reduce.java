80b51f7b79a7231b839d53d96b6ea635c1d38131
Chris-Santan <christopher@santangeli.net>
2024-09-13 16:22:06 -0400

    multiples and reduce first attempt.

---
+++ src/Reduce.java
@@ -0,0 +1,17 @@
+public class Reduce {
+    public static void main(String[] args) {
+        int steps = 0;
+        int number = 100;
+
+        while (number != 0) {
+            if (number % 2 == 0) {
+                steps++;
+                number = number / 2;
+            } else {
+                    steps++;
+                    number = number -1;
+            }
+        }
+        System.out.println(steps);
+    }
+}
